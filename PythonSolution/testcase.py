dfcd9a42a6f9d37f105835d0005ace0531244c0a
FossenWang <31503932+FossenWang@users.noreply.github.com>
2017-12-10 21:10:37 +0800

    2

--- PythonSolution/testcase.py
+++ PythonSolution/testcase.py
@@ -6,9 +6,30 @@ def testTwoSum():
     arglist.append({'nums':[1,2], 'target':3, 'answer':[0,1]})
     arglist.append({'nums':[51,26,44,65,66,79,96], 'target':110, 'answer':[2,4]})
     for arg in arglist:
-        print('expected:',arg['answer'], 'output', twoSum(arg['nums'], arg['target']))
-'''
+        print('expected:',arg['answer'], 'output:', twoSum(arg['nums'], arg['target']))
+
 def testAddTwoNumbers():
+    init = [321, 321, 328, 132, 32, 132]
+    lnl = []
+    for s in init:
+        s = [int(x) for x in str(s)]
+        s.reverse()
+        rl = ListNode(s[0])
+        prel = rl
+        for d in s[1:]:
+            prel.next = ListNode(d)
+            prel = prel.next
+        lnl.append(rl)
+
     arglist = []
-    arglist.append({'l1':[1,2,3], 'l2':[2,3,1], 'answer':[3,5,4]})
-'''
+    arglist.append({'l1':lnl[0], 'l2':lnl[3], 'answer':[3,5,4]})
+    arglist.append({'l1':lnl[1], 'l2':lnl[4], 'answer':[3,5,3]})
+    arglist.append({'l1':lnl[2], 'l2':lnl[5], 'answer':[0,6,4]})
+    for arg in arglist:
+        rl = addTwoNumbers(arg['l1'], arg['l2'])
+        result = [rl.val]
+        while rl.next:
+            rl = rl.next
+            result.append(rl.val)
+
+        print('expected:',arg['answer'], 'output:', result)
